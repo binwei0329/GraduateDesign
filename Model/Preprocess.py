01bfa2d18745da5bd72f371b35e93fe6d1bdb642
binwei0329 <williambinwei@hotmail.com>
2020-07-25 19:03:35 +0800

    minor updates added

--- Model/Preprocess.py
+++ Model/Preprocess.py
@@ -1,8 +1,9 @@
 import numpy as np
 import regex as re
+
 def read_file(file):
     tag_list = []
-    word_ls = []
+    word_list = []
     with open(file) as f:
         for line in f:
             if line == "\n":
@@ -13,7 +14,7 @@ def read_file(file):
                     line = re.sub("\u200b", "X", line)
                 ls = line.split("\t")
 
-                word_ls.append(ls[0])
+                word_list.append(ls[0])
                 tag_list.append(ls[-1])
 
     tag_list = sorted(set(tag_list))
@@ -27,62 +28,41 @@ def read_file(file):
         tag_list[i] = s
     tag_dic = {n:m for m, n in enumerate(tag_list)}
 
-    print(tag_dic)
-    # word_ls = sorted(set(word_ls))
-    print(len(word_ls))
-    # print(word_ls)
-    lsl = []
+    word = []
     temp = ""
     char = []
-    for i in range(len(word_ls)):
-        char_ls = [char for char in word_ls[i]]
+    for i in range(len(word_list)):
+        char_ls = [char for char in word_list[i]]
         char.extend(char_ls)
-        # print(char_ls)
-        # s = "".join(char_ls[0])
         temp += char_ls[0]
-        # print(s)
-    # for i in range(len(word_ls)):
-        if word_ls[i].endswith("0") and i > 0:
-            lsl.append(temp[:-1])
+
+        # Get the segmented words.
+        if word_list[i].endswith("0") and i > 0:
+            word.append(temp[:-1])
             temp = temp[-1] + ""
-            # print(True)
-            # # print(word_ls[i].split())
-            # print([char for char in word_ls[i]])
 
-    lsl = sorted(set(lsl))
-    # print(lsl)
-    print(len(lsl))
+    word = sorted(set(word))
+    word_dic = {v:k for k, v in enumerate(word)}
+    char = sorted(set(char))
 
-    # print(lsl.index("一"))
-    # print(lsl.index("龟"))
-    # print(lsl[lsl.index("一"):lsl.index("龟")])
-    # print(ij)
-    dic = {v:k for k, v in enumerate(lsl)}
-    # print(dic)
-    l = sorted(set(char))
-    print(len(l[l.index("一"):l.index("龟")]))
-    print(len(l))
+    return tag_dic, char, word_dic
 
-
-
-read_file("../Data/Weibo_NER_Corpus.train")
-
-def load_char_embeddings(File):
-    f = open(File,'r')
-    char_dict = {}
+def load_char_embeddings(file):
+    f = open(file,'r')
+    char_embed_dict = {}
     for line in f:
         splitLines = line.split()
         char = splitLines[0]
         wordEmbedding = np.array([float(value) for value in splitLines[1:]])
-        char_dict[char] = wordEmbedding
-    return char_dict
+        char_embed_dict[char] = wordEmbedding
+    return char_embed_dict
 
-g = load_char_embeddings("../Data/vec.txt")
+# g = load_char_embeddings("../Data/vec.txt")
 # print(g)
-i=0
-for k in g.keys():
-    i += 1
-    print(k, "\t", g[k])
-    print(type(k))
-    if i == 10:
-        break
+# i=0
+# for k in g.keys():
+#     i += 1
+#     print(k, "\t", g[k])
+#     print(type(k))
+#     if i == 10:
+#         break
