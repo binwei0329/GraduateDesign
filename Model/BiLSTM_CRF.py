90ec220c6cd156a5cf231d0e9ca8beeedc4c9911
binwei0329 <williambinwei@hotmail.com>
2020-07-29 18:51:40 +0800

    general model added

--- Model/BiLSTM_CRF.py
+++ Model/BiLSTM_CRF.py
@@ -1,11 +1,37 @@
 #! usr/bin/env python3
 # -*- coding:utf-8 -*-
+import tensorflow as tf
+import tensorflow_addons as tf_ad
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.nn import init
-from Model.Preprocess import read_file, load_char_embeddings
 
-tag_dic, char, word_dic = read_file("../Data/Weibo_NER_Corpus.train")
-char_embed_dict = load_char_embeddings("../Data/vec.txt")
+class BiLSTM_CRF(tf.keras.Model):
+    def __init__(self, vocab_size, label_size, hidden_dim, embedding_dim):
+        super(BiLSTM_CRF, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.vocab_size = vocab_size
+        self.label_size = label_size
+
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        self.biLSTM = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(hidden_dim, return_sequences=True))
+        self.dense = tf.keras.layers.Dense(label_size)
+
+        self.transition_params = tf.Variable(tf.random.uniform(shape=(label_size, label_size)))
+        self.dropout = tf.keras.layers.Dropout(0.5)
+
+    # @tf.function
+    def call(self, text, labels=None, training=None):
+        text_lens = tf.math.reduce_sum(tf.cast(tf.math.not_equal(text, 0), dtype=tf.int32), axis=-1)
+        # -1 change 0
+        inputs = self.embedding(text)
+        inputs = self.dropout(inputs, training)
+        logits = self.dense(self.biLSTM(inputs))
+
+        if labels is not None:
+            label_sequences = tf.convert_to_tensor(labels, dtype=tf.int32)
+            log_likelihood, self.transition_params = tf_ad.text.crf_log_likelihood(logits,
+                                                                                   label_sequences,
+                                                                                   text_lens,
+                                                                                   transition_params=self.transition_params)
+            return logits, text_lens, log_likelihood
+        else:
+            return logits, text_lens
