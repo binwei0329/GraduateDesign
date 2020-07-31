34fd3244f07bf4ee1b75cfaa86619c7a825ba37d
binwei0329 <williambinwei@hotmail.com>
2020-07-31 14:47:02 +0800

    releveant methods added

--- Model/BiLSTM_CRF.py
+++ Model/BiLSTM_CRF.py
@@ -33,3 +33,31 @@ def log_sum_exp(vec):
     return max_score + torch.log(torch.sum(torch.exp(vec - max_score_braodcast)))
 
 
+class BiLSTM_CRF(nn.module):
+    def __init__(self, vocab_size, tag_dict, embed_dim, hidden_dim):
+        super(BiLSTM_CRF, self).__init__()
+        self.embed_dim = embed_dim
+        self.vocab_size = vocab_size
+        self.hidden_dim = hidden_dim
+        self.tag_dict = tag_dict
+        self.tag_size = len(tag_dict)
+
+        self.word_embed = nn.Embedding(vocab_size, embed_dim)
+        self.lstm = nn.LSTM(embed_dim, hidden_dim // 2, num_layers=1, bidirectional=True)
+        self.hidden_to_tag = nn.Linear(hidden_dim, self.tag_size)
+
+        self.transitions = nn.Parameter(torch.randn(self.tag_size, self.tag_size))
+        self.transitions.data[tag_dict[STOP_TAG],:] = -10000
+        self.transitions.data[:, tag_dict[STOP_TAG]] = -10000
+
+        self.hidden = self.init_hidden()
+
+
+    def init_hidden(self):
+        return (torch.randn(2, 1, self.hidden_dim // 2), torch.randn(2, 1, self.hidden // 2))
+
+
+    def _forward_pass(self):
+
+
+
