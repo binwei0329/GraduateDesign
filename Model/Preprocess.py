05f7ff475f18b4ee2e66ffd96da4127d055c5458
binwei0329 <williambinwei@hotmail.com>
2020-08-03 11:52:22 +0800

    Preprocess file modified

--- Model/Preprocess.py
+++ Model/Preprocess.py
@@ -1,8 +1,11 @@
 #! usr/bin/env python3
 # -*- coding:utf-8 -*-
 
+import pickle
 import numpy as np
 import regex as re
+import tensorflow as tf
+
 
 def read_file(file):
     tag_list = []
@@ -10,6 +13,7 @@ def read_file(file):
     sentence_list = []
     sentence = []
     tags, tag = [], ""
+
     with open(file) as f:
         for line in f:
             if line == "\n":
@@ -55,8 +59,9 @@ def read_file(file):
     word = sorted(set(word))
     word_dic = {v:k for k, v in enumerate(word)}
     char = sorted(set(char))
+    char_dic = {v:k for k, v in enumerate(char)}
 
-    return tag_dic, char, word_dic, sentence_list, tags
+    return tag_dic, char_dic, word_dic, sentence_list, tags
 
 
 def load_char_embeddings(file):
@@ -68,3 +73,45 @@ def load_char_embeddings(file):
         wordEmbedding = np.array([float(value) for value in splitLines[1:]])
         char_embed_dict[char] = wordEmbedding
     return char_embed_dict
+
+
+def convert_data(file):
+    tag_dic, char_dic, word_dic, sentence_list, tags = read_file(file)
+    data, label = [], []
+    for s in range(len(sentence_list)):
+        d, l = [], []
+        sentence = [char for char in sentence_list[s]]
+        tag = [t for t in tags[s]]
+        for char in sentence:
+            for k in char_dic.keys():
+                if char == k:
+                    d.append(char_dic[k])
+        data.append(d)
+
+        for t in tag:
+            for k in tag_dic.keys():
+                if t == k:
+                    l.append(tag_dic[k])
+        label.append(l)
+
+    with open("../Data/BiLSTM_CRF_data.pkl", "wb") as pkl:
+        pickle.dump(tag_dic, pkl)
+        pickle.dump(char_dic, pkl)
+        pickle.dump(word_dic, pkl)
+        pickle.dump(sentence_list, pkl)
+        pickle.dump(tags, pkl)
+        pickle.dump(data, pkl)
+        pickle.dump(label, pkl)
+
+
+def format_data(data, label):
+    length = []
+    for da in data:
+        length.append(len(da))
+    # avg_len = length // len(data)
+    max_len = max(length)
+    print(max_len)
+    data = tf.keras.preprocessing.sequence.pad_sequences(data, maxlen=max_len, padding="post", value=-1)
+    label = tf.keras.preprocessing.sequence.pad_sequences(label, maxlen=max_len, padding="post", value=-1)
+
+    return data, label
