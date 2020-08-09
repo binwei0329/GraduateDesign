81ad341fb9f72b43dce86af67a7d97b79dd44b8b
binwei0329 <williambinwei@hotmail.com>
2020-08-09 10:30:29 +0800

    minor changes updated

--- Model/Train.py
+++ Model/Train.py
@@ -60,38 +60,39 @@ def traing_BiLSTM_CRF():
     model = BiLSTM_CRF(HIDDEN_DIM, vocab_size, tag_size, EMBED_DIM)
     optimizer = tf.keras.optimizers.Adam(LEARNING_RATE)
 
-    gold_labels = []
-    pred_labels = []
-    # for e in range(EPOCH):
-    #     for i, (data_batch, label_batch) in enumerate(train_dataset):
-    #         # step += 1
-    #         loss, logits, text_lens = train_one_step(model, data_batch, label_batch, optimizer)
-    #         # if e == 19:
-    #             # real_labels = extract_label(text_lens, label_batch)
-    #             # gold_labels.extend(real_labels)
+    # gold_labels = []
+    for e in range(EPOCH):
+        for i, (data_batch, label_batch) in enumerate(train_dataset):
+            # step += 1
+            loss, logits, text_lens = train_one_step(model, data_batch, label_batch, optimizer)
+            # if e == 19:
+                # real_labels = extract_label(text_lens, label_batch)
+                # gold_labels.extend(real_labels)
+
+            if (i + 1) % 5 == 0:
+                print("Epoch:", e, " Loss:", loss.numpy())
+
+    # pred_labels = []
+    # gold_labels = []
+    # for i, (data, label) in enumerate(train_dataset):
+    #     # print(np.array(label).shape)
+    #     gold_labels.extend(np.array(label))
+    #     predictions = predict(model, label, data)
+    #     # print(len(predictions))
+    #     pred_labels.extend(predictions)
     #
-    #         if (i + 1) % 5 == 0:
-    #             print("Epoch:", e, " Loss:", loss.numpy())
+    # # 为了解决实际标签和预测标签不一致的问题，该问题由随机排序导致
+    # for s in range(len(pred_labels)):
+    #     pred_len = len(pred_labels[s])
+    #     gold_label = gold_labels[s]
+    #     effective_part = gold_label[:pred_len]
+    #     gold_labels[s] = effective_part
 
-    gold_labels = []
-    for i, (data, label) in enumerate(train_dataset):
-        # print(np.array(label).shape)
-        gold_labels.extend(np.array(label))
-        predictions = predict(model, label, data)
-        # print(len(predictions))
-        pred_labels.extend(predictions)
-
-
-    for s in range(len(pred_labels)):
-        pred_len = len(pred_labels[s])
-        gold_label = gold_labels[s]
-        effective_part = gold_label[:pred_len]
-        gold_labels[s] = effective_part
-
-    for i in range(len(pred_labels)):
-        print(i, "Pred len: ", len(pred_labels[i]), "Gold len: ", (len(gold_labels[i])))
+    # for i in range(len(pred_labels)):
+    #     print(i, "Pred len: ", len(pred_labels[i]), "Gold len: ", (len(gold_labels[i])))
     # print(len(gold_labels))
-    # with open("labels.pkl", "wb") as file:
+
+    # with open("labels2.pkl", "wb") as file:
     #     pickle.dump(pred_labels, file)
     #     pickle.dump(gold_labels, file)
 
@@ -109,23 +110,37 @@ def traing_BiLSTM_CRF():
 
     # test_dataset = tf.data.Dataset.from_tensor_slices((data_test, label_test))
     # test_dataset = test_dataset.shuffle(len(data_test)).batch(64, drop_remainder=True)
-    # # model = BiLSTM_CRF(HIDDEN_DIM, vocab_size_t, tag_size_t, EMBED_DIM)
-    # # optimizer = tf.keras.optimizers.Adam(LEARNING_RATE)
 
-    # with open ("../Data/weiboNER_2nd_conll.test.pkl", "rb") as file_test:
-    #     tag_dic_t = pickle.load(file_test)
-    #     char_dic_t = pickle.load(file_test)
-    #     word_dic_t = pickle.load(file_test)
-    #     sentence_list_t = pickle.load(file_test)
-    #     tags_t = pickle.load(file_test)
-    #     data_test_t = pickle.load(file_test)
-    #     label_test_t = pickle.load(file_test)
-    # data_test_t, label_test_t = format_data(data_test_t, label_test_t)
-    # vocab_size_t = len(char_dic_t)
-    # tag_size_t = len(tag_dic_t)
-    # test_dataset_t = tf.data.Dataset.from_tensor_slices((data_test_t, label_test_t))
-    # test_dataset_t = test_dataset_t.shuffle(len(data_test_t)).batch(128, drop_remainder=True)
-    # tf.print(best_acc_t)
+
+    with open ("../Data/weiboNER_2nd_conll.test.pkl", "rb") as file_test:
+        tag_dic_test = pickle.load(file_test)
+        char_dic_test = pickle.load(file_test)
+        word_dic_test = pickle.load(file_test)
+        sentence_list_test = pickle.load(file_test)
+        tags_test = pickle.load(file_test)
+        data_test = pickle.load(file_test)
+        label_test = pickle.load(file_test)
+    data_test_t, label_test_t = format_data(data_test, label_test)
+    vocab_size_test = len(char_dic_test)
+    tag_size_test = len(tag_dic_test)
+    test_dataset = tf.data.Dataset.from_tensor_slices((data_test, label_test))
+    test_dataset = test_dataset.shuffle(len(data_test_t)).batch(64, drop_remainder=True)
+
+    gold_labels_test = []
+    pred_labels_test = []
+
+    for i, (data_test, label_test) in enumerate(test_dataset):
+        # print(np.array(label).shape)
+        gold_labels_test.extend(np.array(label_test))
+        predictions_test = predict(model, label_test, data_test)
+        # print(len(predictions))
+        pred_labels_test.extend(predictions_test)
+
+
+    with open("labels_test.pkl", "wb") as file:
+        pickle.dump(pred_labels_test, file)
+        pickle.dump(gold_labels_test, file)
+
 
 if __name__ == "__main__":
     traing_BiLSTM_CRF()
