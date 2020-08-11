ad267f5c868a2d6a6a0b6e95f5d3de6c6af24eb5
binwei0329 <williambinwei@hotmail.com>
2020-08-11 14:24:11 +0800

    model description added

--- Model/BiLSTM_CRF_Train.py
+++ Model/BiLSTM_CRF_Train.py
@@ -1,5 +1,11 @@
 #! usr/bin/env python3
 # -*- coding:utf-8 -*-
+"""
+Graduate Project: Named Entity Recognition Task in Chinese Media
+Author: Bin Wei
+Description: This is part of my graduate project, which tries to explore some
+            models targeting NER tasks in Chinese social media.
+"""
 
 import os
 import pickle
---
+++ Model/Seq2Seq.py
@@ -0,0 +1,9 @@
+#! usr/bin/env python3
+# -*- coding:utf-8 -*-
+"""
+Graduate Project: Named Entity Recognition Task in Chinese Media
+Author: Bin Wei
+Description: This is part of my graduate project, which tries to explore some
+            models targeting NER tasks in Chinese social media.
+"""
+
