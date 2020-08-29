609a012ca110dbdfd85d8b129a0da4172cd6ca30
binwei0329 <williambinwei@hotmail.com>
2020-08-29 17:29:51 +0800

    code resvised

--- Model/BiLSTM_CRF_Train.py
+++ Model/BiLSTM_CRF_Train.py
@@ -38,7 +38,8 @@ def load_data(file, batch_size):
     return dataset, vocab_size, tag_size
 
 
-def load_data_helper(data_op, batch_size):
+def load_data_helper(batch_size):
+    # data_op,
     """
     This method returns the formatted dataset, vocab size and tag size of different files.
     :param op: the options
@@ -46,59 +47,59 @@ def load_data_helper(data_op, batch_size):
     """
     # If the option is "train", we apply undersampling and oversampling technique to get
     # bigger datasets.
-    if data_op == "train":
-        with open("../PickleFiles/Chinese_Weibo_NER_Corpus_original_train.pkl", "rb") as file:
-            _ = pickle.load(file)
-            _ = pickle.load(file)
-            _ = pickle.load(file)
-            _ = pickle.load(file)
-            data = pickle.load(file)
-            label = pickle.load(file)
+    # if data_op == "train":
+    with open("../PickleFiles/Chinese_Weibo_NER_Corpus_original_train.pkl", "rb") as file:
+        _ = pickle.load(file)
+        _ = pickle.load(file)
+        _ = pickle.load(file)
+        _ = pickle.load(file)
+        data = pickle.load(file)
+        label = pickle.load(file)
 
-        length = len(data)
+    length = len(data)
 
-        with open("../PickleFiles/Chinese_Weibo_NER_Corpus_train.pkl", "rb") as file_train:
-            tag_dic = pickle.load(file_train)
-            char_dic = pickle.load(file_train)
-            sentence_list = pickle.load(file_train)
-            tags = pickle.load(file_train)
-            data_train = pickle.load(file_train)
-            label_train = pickle.load(file_train)
+    with open("../PickleFiles/Chinese_Weibo_NER_Corpus_train.pkl", "rb") as file_train:
+        tag_dic = pickle.load(file_train)
+        char_dic = pickle.load(file_train)
+        sentence_list = pickle.load(file_train)
+        tags = pickle.load(file_train)
+        data_train = pickle.load(file_train)
+        label_train = pickle.load(file_train)
 
-        data_duplicate = data_train[length:]
-        label_duplicate = label_train[length:]
+    data_duplicate = data_train[length:]
+    label_duplicate = label_train[length:]
 
-        # Oversampling the named entities.
-        for i in range(3):
-            data_train.extend(data_duplicate)
-            label_train.extend(label_duplicate)
+    # Oversampling the named entities.
+    for i in range(3):
+        data_train.extend(data_duplicate)
+        label_train.extend(label_duplicate)
 
-        # Undersampling the data without named entities.
-        for i in range(length):
-            label = label_train[i]
-            dic = Counter(label)
-            if dic[16] == len(label):
-                del label_train[i]
-                del data_train[i]
+    # Undersampling the data without named entities.
+    for i in range(length):
+        label = label_train[i]
+        dic = Counter(label)
+        if dic[16] == len(label):
+            del label_train[i]
+            del data_train[i]
 
-        data_train, label_train = format_data(data_train, label_train, tag_dic)
-        vocab_size = len(char_dic)
-        tag_size = len(tag_dic)
+    data_train, label_train = format_data(data_train, label_train, tag_dic)
+    vocab_size = len(char_dic)
+    tag_size = len(tag_dic)
 
-        dataset = tf.data.Dataset.from_tensor_slices((data_train, label_train))
-        dataset = dataset.shuffle(len(data_train)).batch(batch_size, drop_remainder=True)
+    dataset = tf.data.Dataset.from_tensor_slices((data_train, label_train))
+    dataset = dataset.shuffle(len(data_train)).batch(batch_size, drop_remainder=True)
 
-    elif data_op == "dev":
-        file = "../PickleFiles/Chinese_Weibo_NER_Corpus_dev.pkl"
-        dataset, vocab_size, tag_size = load_data(file, batch_size)
-
-    elif data_op == "test":
-        file = "../PickleFiles/Chinese_MSRA_NER_Corpus_test.pkl"
-        dataset, vocab_size, tag_size = load_data(file, batch_size)
-
-    else:
-        file = "../PickleFiles/Chinese_Weibo_NER_Corpus_original_train.pkl"
-        dataset, vocab_size, tag_size = load_data(file, batch_size)
+    # elif data_op == "dev":
+    #     file = "../PickleFiles/Chinese_Weibo_NER_Corpus_dev.pkl"
+    #     dataset, vocab_size, tag_size = load_data(file, batch_size)
+    #
+    # elif data_op == "test":
+    #     file = "../PickleFiles/Chinese_MSRA_NER_Corpus_test.pkl"
+    #     dataset, vocab_size, tag_size = load_data(file, batch_size)
+    #
+    # else:
+    #     file = "../PickleFiles/Chinese_Weibo_NER_Corpus_original_train.pkl"
+    #     dataset, vocab_size, tag_size = load_data(file, batch_size)
 
     return dataset, vocab_size, tag_size
 
@@ -131,7 +132,7 @@ def save_labels(model, dataset, name):
             pickle.dump(gold_labels, file)
 
 
-def train_BiLSTM_CRF(batch_op):
+def train_BiLSTM_CRF(dataset, vocab_size, tag_size):
     """
     This method trains the model and generates the predictions.
     """
@@ -139,32 +140,20 @@ def train_BiLSTM_CRF(batch_op):
     UNIT_NUM = 64
     EPOCH = 20
     LEARNING_RATE = 0.005
-    BATCH_SIZE = {"weibo":64, "msra":10240, "twitter":2048}
-    char_embed_dict = load_char_embeddings("../Data/vec.txt")
-
-    # Use the augmented training set to train the model.
-    train_dataset, vocab_size, tag_size = load_data_helper("train", BATCH_SIZE[batch_op])
     model = BiLSTM_CRF(UNIT_NUM, vocab_size, tag_size, EMBED_DIM)
     optimizer = tf.keras.optimizers.Adam(LEARNING_RATE)
+
+    # Use the augmented training set to train the model.
+    # train_dataset, vocab_size, tag_size = load_data_helper(BATCH_SIZE[batch_op])
     for e in range(EPOCH):
-        for i, (data_batch, label_batch) in enumerate(train_dataset):
-            print(label_batch)
+        # for i, (data_batch, label_batch) in enumerate(train_dataset):
+        for i, (data_batch, label_batch) in enumerate(dataset):
             loss, logits, text_lens = train_one_step(model, data_batch, label_batch, optimizer)
-            print(logits)
             if (i + 1) % 10 == 0:
                 print("Epoch:", e + 1, "Batch:", i + 1, "Loss:", loss.numpy())
 
-    # Generate the predictions on the following datasets and write them into corresponding pickle files.
-    save_labels(model, train_dataset, "trainset")
+    return model
 
-    dev_dataset, _, _ = load_data_helper("dev", BATCH_SIZE[batch_op])
-    save_labels(model, dev_dataset, "devset")
-
-    test_dataset, _, _ = load_data_helper("test", BATCH_SIZE[batch_op])
-    save_labels(model, test_dataset, "testset")
-
-    # origin_train_dataset = load_data_helper("origin_train")
-    # store_labels(model, origin_train_dataset, t)
 
 def report_perfomence(arg):
     """
@@ -181,7 +170,39 @@ def report_perfomence(arg):
     print(arg, "performance precision: %.2f recall: %.2f f1score: %.2f" %(precision*100, recall*100, f1*100))
 
 
+def test_model(model, data_op, trainset):
+    if data_op == "msra":
+        save_labels(model, trainset, "msra_train")
+        test_dataset, _, _ = load_data("../PickleFiles/Chinese_MSRA_NER_Corpus_test.pkl", batch_size=10240)
+        save_labels(model, test_dataset, "msra_test")
+
+    elif data_op == "twitter":
+        save_labels(model, trainset, "twitter_train")
+        test_dataset, _, _ = load_data("../PickleFiles/English_Twitter_NER_Corpus_train.pkl", batch_size=2048)
+        save_labels(model, test_dataset, "weibo_test")
+
+    else:
+        if data_op == "weibo":
+            save_labels(model, trainset, "weibo_train")
+        else:
+            save_labels(model, trainset, "weibo_train_origin")
+
+        dev_dataset, _, _ = load_data("../PickleFiles/Chinese_Weibo_NER_Corpus_dev.pkl", batch_size=64)
+        save_labels(model, dev_dataset, "weibo_dev")
+
+        test_dataset, _, _ = load_data("../PickleFiles/Chinese_Weibo_NER_Corpus_test.pkl", batch_size=64)
+        save_labels(model, test_dataset, "weibo_test")
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
+    pass
+
     # cond1 = os.path.exists("../Data/bilstm_crf_labels_testset.pkl")
     # cond2 = os.path.exists("../Data/bilstm_crf_labels_trainset.pkl")
     # cond3 = os.path.exists("../Data/bilstm_crf_labels_devset.pkl")
@@ -191,4 +212,4 @@ if __name__ == "__main__":
     #     report_perfomence("testset")
     #
     # else:
-        train_BiLSTM_CRF()
+    #     train_BiLSTM_CRF()
