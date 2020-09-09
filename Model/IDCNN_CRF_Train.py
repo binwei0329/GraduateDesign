0017ba605beed6e0774e2822b1f0992b42576ec2
binwei0329 <williambinwei@hotmail.com>
2020-09-09 11:06:12 +0800

    train file updated

--- Model/IDCNN_CRF_Train.py
+++ Model/IDCNN_CRF_Train.py
@@ -8,9 +8,11 @@ Description: This is part of my graduate project, which tries to explore some
 """
 
 import os
+import pickle
+import numpy as np
 import tensorflow as tf
-from Model.BiLSTM_CRF_Train import load_data, load_data_helper, save_labels
-from Model.IDCNN_CRF import IDCNN_CRF, train_one_step
+from Model.IDCNN_CRF import IDCNN_CRF, train_one_step, predict
+from Model.BiLSTM_CRF_Train import load_data, load_data_helper
 
 def train_IDCNN_CRF(dataset, vocab_size, tag_size, epoch):
     """
@@ -33,6 +35,60 @@ def train_IDCNN_CRF(dataset, vocab_size, tag_size, epoch):
     return model
 
 
+def save_labels(model, dataset, name):
+    """
+    The method uses the trained model to generate predicted labels and
+    writes both predicted labels and gold labels into corresponding pickle files.
+    :param model: trained model
+    :param dataset: dataset on which the model makes predictions
+    :param op: name to choose
+    """
+    pred_labels = []
+    gold_labels = []
+    for i, (data, label) in enumerate(dataset):
+        gold_labels.extend(np.array(label))
+        predictions = predict(model, label, data)
+        pred_labels.extend(predictions)
+
+    for s in range(len(pred_labels)):
+        pred_len = len(pred_labels[s])
+        gold_label = gold_labels[s]
+        effective_part = gold_label[:pred_len]
+        gold_labels[s] = effective_part
+
+
+    filename = "../PickleFiles/Labels_Idcnn_crf_" + name + ".pkl"
+    if os.path.exists(filename) == False:
+        with open(filename, "wb") as file:
+            pickle.dump(pred_labels, file)
+            pickle.dump(gold_labels, file)
+
+
+def test_model(model, data_op, trainset):
+    if data_op == "weibo":
+        save_labels(model, trainset, "weibo_train")
+        dev_dataset, _, _, _ = load_data("../PickleFiles/Chinese_Weibo_NER_Corpus_dev.pkl", batch_size=64)
+        save_labels(model, dev_dataset, "weibo_dev")
+
+        test_dataset, _, _, _ = load_data("../PickleFiles/Chinese_Weibo_NER_Corpus_test.pkl", batch_size=64)
+        save_labels(model, test_dataset, "weibo_test")
+
+    else:
+        save_labels(model, trainset, "weibo_train_origin")
+        dev_dataset, _, _, _ = load_data("../PickleFiles/Chinese_Weibo_NER_Corpus_dev_origin.pkl", batch_size=64)
+        save_labels(model, dev_dataset, "weibo_dev_origin")
+
+        test_dataset, _, _, _ = load_data("../PickleFiles/Chinese_Weibo_NER_Corpus_test_origin.pkl", batch_size=64)
+        save_labels(model, test_dataset, "weibo_test_origin")
+
+
 if __name__ == "__main__":
-    dataset, vocab_size, tag_size, tag_dic = load_data_helper(64)
-    train_IDCNN_CRF(dataset, vocab_size, tag_size, 20)
+    weibo_train, vocab_size, tag_size, tag_dic = load_data_helper(64)
+    model = train_IDCNN_CRF(weibo_train, vocab_size, tag_size, 5)
+    test_model(model, "weibo", weibo_train)
+
+    # weibo_train_origin, vocab_size_o, _, _ = load_data("../PickleFiles/Chinese_Weibo_NER_Corpus_train_origin.pkl", batch_size=64)
+    # model_o = train_IDCNN_CRF(weibo_train_origin, vocab_size_o, tag_size, epoch=20)
+    # test_model(model_o, "weibo_origin", weibo_train_origin)
+
+    print("Model trained and predictions given.")
